30c8e72b38e02cc7495ab06a414b99d1ea6d4e7f
counter <counter>
2021-05-02 22:24:14 +0200

    add permutation and substitution empty skeletton files

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "src/core/include/permutation.h"
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+    // printf() displays the string inside quotation
+    printf("Hello, World!\n");
+    do_permutation();
    return 0;
 }
---
+++ src/core/permutation/permutation.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int do_permutation ()
+{
+    printf("not finished\n");
+}

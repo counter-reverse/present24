5110733bf9dfcb62d55e7739b769be1d123fb0be
iqbalOket <oketiqbal@gmail.com>
2021-05-08 11:34:15 -0700

    Main

---
+++ Fonction.h
@@ -0,0 +1,11 @@
+#ifndef FONCTION_H_INCLUDED
+#define FONCTION_H_INCLUDED
+
+char substitution(char val);
+
+void conversion();
+void permutation(char TabEntrer[]);
+int recherche(int val);
+void printTab(char tab[]);
+
+#endif // FONCTION_H_INCLUDED
